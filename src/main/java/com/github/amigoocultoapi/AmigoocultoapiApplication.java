8b6f249dfd26522eb61279b95b69d882ea6e7760
Thiago Seus <thiago.seus@yahoo.com.br>
2022-07-23 19:18:46 -0300

    Primeiro commit

---
+++ src/main/java/com/github/amigoocultoapi/AmigoocultoapiApplication.java
@@ -0,0 +1,13 @@
+package com.github.amigoocultoapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmigoocultoapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmigoocultoapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/amigoocultoapi/AmigoocultoapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.amigoocultoapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmigoocultoapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
